85016e6cfe4ab6adfc2bf573c5bcd3bb0f6496d3
Barbara Hernandez <barbarahernandezrivero@MacBook-Air.local>
2021-03-19 22:34:00 -0600

    Updates

---
+++ Practicas_Individuales/datatype.py
@@ -0,0 +1,11 @@
+# el signo de gato sirve para poner comentarios
+# string, se refiere a una secuencia "cadena" de caracteres por ejemplo:
+# Asì se expresa una string "Hello world" # con comillas dobles
+# 'Hello world' # o con comillas sencillas
+# strings
+print("Hello World")
+print('hello world')
+print('''hhello wworld''')
+print(type("hello"))
+
+
